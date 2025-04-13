b0013f3adf7c44d29bdd2bf244e87dbe7f260e8a
prasad <prasadpatila99@gmail.com>
2025-04-13 15:03:27 +0530

    backend added

---
+++ codeByteLearning/src/main/java/com/codebyte/CodeByteLearningApplication.java
@@ -0,0 +1,13 @@
+package com.codebyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeByteLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeByteLearningApplication.class, args);
+	}
+
+}
---
+++ codeByteLearning/src/test/java/com/codebyte/CodeByteLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeByteLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
