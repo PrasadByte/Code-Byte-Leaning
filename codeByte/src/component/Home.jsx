9f0e1c009f045225d1596f14a889bfbc99859476
prasad <prasadpatila99@gmail.com>
2025-04-18 23:30:32 +0530

    card added

--- codeByte/src/component/Home.jsx
+++ codeByte/src/component/Home.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import '../styles/Home.css'
+import React from "react";
+import "../styles/Home.css";
 
 export default function Home() {
   return (
     <div className="home-container">
       <h1>Hello, what do You Want To Learn?</h1>
       <div className="input-group">
-        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username with two button addons" />
-        <button className="btn btn-outline-secondary" type="button">Button</button>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Recipient's username"
+          aria-label="Recipient's username with two button addons"
+        />
+        <button className="btn btn-outline-secondary" type="button">
+          Button
+        </button>
       </div>
-
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </div>
-
-
-  )
+  );
 }
