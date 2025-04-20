ee6b0a8e300c9ece903b67bf6393e49d6e19089c
prasad <prasadpatila99@gmail.com>
2025-04-20 14:00:29 +0530

    card more but not reponsive

--- codeByte/src/component/Home.jsx
+++ codeByte/src/component/Home.jsx
@@ -15,9 +15,10 @@ export default function Home() {
         <button className="btn btn-outline-secondary" type="button">
           Button
         </button>
+        <h5 id="courses-main">Courses</h5>
       </div>
       <div className="courses">
-        <h5 id="courses-main">Courses</h5>
+        
         <div className="card" style={{ width: "18rem" }}>
           <img
             src="https://via.placeholder.com/150"
@@ -69,6 +70,92 @@ export default function Home() {
             </a>
           </div>
         </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://via.placeholder.com/150"
+            className="card-img-top"
+            alt="Placeholder"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Java Script</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://via.placeholder.com/150"
+            className="card-img-top"
+            alt="Placeholder"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Java Script</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://via.placeholder.com/150"
+            className="card-img-top"
+            alt="Placeholder"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Java Script</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://via.placeholder.com/150"
+            className="card-img-top"
+            alt="Placeholder"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Java Script</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://via.placeholder.com/150"
+            className="card-img-top"
+            alt="Placeholder"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Java Script</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
