0db23c814c6558a71a7a5182e7cda241d4ad96ed
prasad <prasadpatila99@gmail.com>
2025-04-21 23:41:24 +0530

    working is done on dao

--- codeByteLearning/src/main/java/com/codebyte/dao/Trainerdao.java
+++ codeByteLearning/src/main/java/com/codebyte/dao/Trainerdao.java
@@ -1,5 +1,11 @@
 package com.codebyte.dao;
 
-public interface Trainerdao {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codebyte.entities.Trainer;
+
+public interface Trainerdao extends JpaRepository<Trainer, Integer> {
+	// Custom query methods can be defined here if needed
+	// For example, findByEmail(String email) to find a trainer by their email
 
 }
